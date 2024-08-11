ac5d06d88c1a1f72acdf19c62e097fae9da7cd2b
be_ahmadsam <ahmadsam2882@gmail.com>
2024-08-12 01:46:55 +0530

    Basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,26 @@ function App() {
 
   return (
     <>
-      <h1>Hii</h1>
-      
-    </>
+      <h2>BMI calculator</h2>
+      <form>
+        <div>
+          <label >Weight</label>
+          <input type="text" placeholder="Enter your Weight in kg"  />
+        </div>
+        <div>
+          <label >Height</label>
+          <input type="text" placeholder="Enter your Height in cm"  />
+        </div>
+        <div className="button">
+          <button className="btn" type='submit'>Submit</button>
+          <button className="btn btn-outline" type='submit'>Reload</button>
+        </div>
+        <div className="center">
+          <h3>Your BMI is: btaenge</h3>
+          <p>message to be printed</p>
+        </div>
+      </form>
+  </>
   )
 }
 
